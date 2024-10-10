5d49a9772bff537cdead3a950635e14a276f1fe1
Goodchild21 <Goodchild_21@mail.ru>
2024-10-10 16:08:35 +0300

    последнее

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,6 @@ from src.database import Base
 from src.todo.models import *
 from src.auth.models import *
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -23,7 +22,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-from src.todo.models import *
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
