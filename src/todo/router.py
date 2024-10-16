e3cd055ab4d470d3d343c504709adce4e8fed1b9
Goodchild21 <Goodchild_21@mail.ru>
2024-10-17 01:00:50 +0300

    last

--- src/todo/router.py
+++ src/todo/router.py
@@ -6,23 +6,36 @@ from src.todo.schemas import ToDoBase, ToDo
 from src.auth.utils import oauth2_scheme
 
 
+
 router = APIRouter(prefix="/todo", tags=["todo"])
 
+
+#
 @router.get('/', response_model=list[ToDoBase])
-async def get_todos():
+async def get_all_todos():
     with SessionLocal() as session:
-        todos = utils.get_all_todos(session)
+        todos = utils.get_todos(session)
     return todos
 
 
+#
+@router.get('/{my_todos}', response_model=list[ToDoBase])
+async def get_all_my_todos(token = Depends(oauth2_scheme)):
+    with SessionLocal() as session:
+        todos = utils.get_my_todos(session, token)
+    return todos
+
+
+#
 @router.post('/', response_model=ToDo)
 async def create_todo(todo: ToDoBase, token = Depends(oauth2_scheme)):
     with SessionLocal() as session:
         session.expire_on_commit = False
-        todo = utils.create_todo(session, todo)
+        todo = utils.create_todo(session, todo, token)
     return todo
 
 
+#
 @router.put("/{todo_id}", response_model=ToDoBase)
 async def update_todo(todo_id: int, todo: ToDoBase, token = Depends(oauth2_scheme)):
     with SessionLocal() as session:
@@ -31,6 +44,7 @@ async def update_todo(todo_id: int, todo: ToDoBase, token = Depends(oauth2_schem
     return todo
 
 
+#
 @router.delete('/{todo_id}', response_model=ToDo)
 async def delete_todo(todo_id: int, token = Depends(oauth2_scheme)):
     with SessionLocal() as session:
--- src/todo/utils.py
+++ src/todo/utils.py
@@ -1,16 +1,21 @@
 from sqlalchemy.orm import Session
+
 from src.todo.models import ToDo
-import src.todo.schemas as schemas
+from src.auth.utils import get_current_user
+from src.todo.schemas import ToDoBase
 
 
 #NEW
-def create_todo(db: Session, todo: schemas.ToDoBase) -> ToDo:
+def create_todo(db: Session, todo: ToDoBase, token) -> ToDo:
+    user = get_current_user(db, token)
     todo = ToDo(
         title=todo.title,
+        user_id=user.id
     )
     db.add(todo)
     db.commit()
     return todo
+
 
 #UPDATE
 def update_todo(db: Session, new_todo: ToDo, todo_id: int) -> ToDo:
@@ -20,13 +25,17 @@ def update_todo(db: Session, new_todo: ToDo, todo_id: int) -> ToDo:
     db.commit()
     return todo
 
+
 #SELECT ALL
-def get_all_todo(db: Session) -> list[ToDo]:
+def get_todos(db: Session) -> list[ToDoBase]:
     return db.query(ToDo).all()
 
-#SELECT ID
-def get_todo_by_id(db: Session, todo_id: int) -> ToDo:
-    return db.query(ToDo).filter(ToDo.id == todo_id).first()
+
+#SELECT ALL MY TODOS
+def get_my_todos(db: Session, token) -> ToDo:
+    user = get_current_user(db, token)
+    return db.query(ToDo).filter(ToDo.user_id == user.id).all()
+
 
 #DEL
 def delete_todo_by_id(db: Session, todo_id: int) -> ToDo:
